442ea97e2d642c08b8c872d1b89ffe615660b000
kgaber1 <42582537+kgaber1@users.noreply.github.com>
2018-08-23 12:18:08 -0400

    Add files via upload

--- OldMacDonald2.java
+++ OldMacDonald2.java
@@ -11,26 +11,27 @@
 public class OldMacDonald2 
 {
     
-    public static void eieio() 
+    public static void eieio() //Give our song lines 1 of the verse.
     { 
         System.out.println("Old MacDonald had a farm, E I E I O.");
     }
 
 
-    public static void hadA(String animal) 
+    public static void hadA(String animal)  //Give our song line 2 of the verse and insert animal on the farm.
+    
     { 
         System.out.println("And on that farm he had a " + animal + ", E I E I O.");
     }
 
 
-    public static void withA(String sound) 
+    public static void withA(String sound) //Create lines 3 and 4 and input the sound the animal makes.
     { 
         System.out.println("With a " + sound + " " + sound +  " here and a " + sound + " " + sound +  " there.");
         System.out.println("Here a " + sound + ", there a " + sound + ", everywhere a " + sound + ", " + sound + ".");  
     }
 
     
-    public static void singIt(String animal, String sound) 
+    public static void singIt(String animal, String sound) //sings a verse and passes in the inputted string.
     {
         eieio();
         hadA(animal);
@@ -40,9 +41,9 @@ public class OldMacDonald2
     }
 
     
-    public static void main(String[] args) 
+    public static void main(String[] args) //Allows you to put in an animal as a string for it to be inputted into the verse.
     {
-        System.out.println("Old MacDonald Had A Farm\n"); // Give our        song a title
+        System.out.println("Old MacDonald Had A Farm\n"); // Give our song a title
 
         singIt("duck", "quack");
         singIt("cow", "moo");
